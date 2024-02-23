5a206a05bda0bb4f2fc2d5e26baec158c6d20cf8
Michael <enjoyweaver@gmail.com>
2024-02-23 16:44:29 -0600

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,64 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-20" style={{ backgroundColor: "#f2f2f2" }}>
+      <div className="container flex items-center justify-between px-4 py-8">
+        <div className="flex items-center">
+          <img
+            src="/logo.png"
+            alt="Twitter Icon"
+            width="50"
+            height="50"
+            className="mr-4"
+          />
+          <p style={{ color: "var(--header-color)" }}>Crypto Policy DAO</p>
+        </div>
+        <div className="flex items-center">
+          <a
+            href="https://twitter.com/CryptoPolicyDAO"
+            target="_blank"
+            rel="noreferrer"
+            className="mr-4 hover:text-blue-500"
+          >
+            <img
+              src="/twitter.webp"
+              alt="Twitter Icon"
+              width="30"
+              height="30"
+              className=""
+            />
+          </a>
+          <a
+            href="https://github.com/CryptoPolicyDAO"
+            target="_blank"
+            rel="noreferrer"
+            className="mr-4 hover:text-black"
+          >
+            <img
+              src="/github.png"
+              alt="GitHub Icon"
+              width="30"
+              height="30"
+              className=""
+            />
+          </a>
+          <a
+            href="https://discord.gg/fX7UaC4A5M"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-purple-500" // Adjust hover color as needed
+          >
+            <img
+              src="/discord.png"
+              alt="Discord Icon"
+              width="30"
+              height="30"
+              className=""
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import videoFile from "/3DLogo.mp4";
 import { useState } from "react";
 
@@ -21,33 +22,90 @@ export default function Home() {
 
   `;
   return (
-    <div className="container pt-20">
-      {/* Mobile layout (max-width: 768px) */}
-      <div className="md:hidden">
-        <div className="container mx-auto pt-20 pb-20 justify-center items-center">
-          <p
-            className="header pb-10 "
-            style={{ color: "var(--header-color)", fontSize: "2rem" }}
-          >
-            Crypto Policy DAO
-          </p>
-          <div className="video-center video-container">
-            <video autoPlay loop muted width="300" height="400">
+    <div>
+      <div className="container pt-20">
+        {/* Mobile layout (max-width: 768px) */}
+        <div className="md:hidden">
+          <div className="container mx-auto pt-20 pb-20 justify-center items-center">
+            <p
+              className="header pb-10 "
+              style={{ color: "var(--header-color)", fontSize: "2rem" }}
+            >
+              Crypto Policy DAO
+            </p>
+            <div className="video-center video-container">
+              <video autoPlay loop muted width="300" height="400">
+                <source src={videoFile} type="video/mp4" />
+              </video>
+            </div>
+            <div className="text-center">
+              <br />
+              <p
+                className="sub-header text-center "
+                style={{ color: "var(--header-color)", fontSize: "1.5rem" }}
+              >
+                Web3-friendly Regulatory Think Tank
+              </p>
+
+              <br />
+              <div className="text-center pl-2 pr-2">
+                <p className="content1">
+                  Our mission is to be a platform for policymakers and
+                  blockchain builders to collaborate on the technical and
+                  regulatory research of blockchain technologies that promote a
+                  web3-friendly regulatory environment. The speed of blockchain
+                  innovation is outpacing the regulatory environment and leaving
+                  legislators behind in understanding and creating web3-friendly
+                  regulation.
+                </p>
+                <br />
+                <p className="content2">
+                  The underlying technology propelling the blockchain will
+                  continuously advance regardless of any political regulation,
+                  and for us to capture the benefits of emerging and innovative
+                  blockchain technology, we need to understand how and where
+                  regulation of the blockchain connect.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Desktop layout (min-width: 768px) */}
+        <div className="pt-10 hidden md:flex">
+          <div className="w-1/2 text-center pl-20 pt-5">
+            <video
+              autoPlay
+              loop
+              muted
+              width="490"
+              height="580"
+              onMouseEnter={() => setIsZoomed(true)}
+              onMouseLeave={() => setIsZoomed(false)}
+              className={isZoomed ? "zoomed" : ""}
+            >
               <source src={videoFile} type="video/mp4" />
             </video>
           </div>
-          <div className="text-center">
+          <div className="w-1/2 text-left max-w-600">
+            <p
+              className="text-left text-2xl"
+              style={{ color: "var(--header-color)", fontSize: "2rem" }}
+            >
+              Crypto Policy DAO
+            </p>
             <br />
             <p
-              className="sub-header text-center "
+              className="text-left text-xl"
               style={{ color: "var(--header-color)", fontSize: "1.5rem" }}
             >
               Web3-friendly Regulatory Think Tank
             </p>
-
             <br />
-            <div className="text-center pl-2 pr-2">
-              <p className="content1">
+            <div className="text-left">
+              <p
+                style={{ color: "var(--description-color)", maxWidth: "550px" }}
+              >
                 Our mission is to be a platform for policymakers and blockchain
                 builders to collaborate on the technical and regulatory research
                 of blockchain technologies that promote a web3-friendly
@@ -56,7 +114,7 @@ export default function Home() {
                 behind in understanding and creating web3-friendly regulation.
               </p>
               <br />
-              <p className="content2">
+              <p style={{ color: "var(--content-color)", maxWidth: "550px" }}>
                 The underlying technology propelling the blockchain will
                 continuously advance regardless of any political regulation, and
                 for us to capture the benefits of emerging and innovative
@@ -66,60 +124,9 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <style>{inlineStyles}</style>
       </div>
-
-      {/* Desktop layout (min-width: 768px) */}
-      <div className="pt-10 hidden md:flex">
-        <div className="w-1/2 text-center pl-20 pt-5">
-          <video
-            autoPlay
-            loop
-            muted
-            width="490"
-            height="580"
-            onMouseEnter={() => setIsZoomed(true)}
-            onMouseLeave={() => setIsZoomed(false)}
-            className={isZoomed ? "zoomed" : ""}
-          >
-            <source src={videoFile} type="video/mp4" />
-          </video>
-        </div>
-        <div className="w-1/2 text-left max-w-600">
-          <p
-            className="text-left text-2xl"
-            style={{ color: "var(--header-color)", fontSize: "2rem" }}
-          >
-            Crypto Policy DAO
-          </p>
-          <br />
-          <p
-            className="text-left text-xl"
-            style={{ color: "var(--header-color)", fontSize: "1.5rem" }}
-          >
-            Web3-friendly Regulatory Think Tank
-          </p>
-          <br />
-          <div className="text-left">
-            <p style={{ color: "var(--description-color)", maxWidth: "550px" }}>
-              Our mission is to be a platform for policymakers and blockchain
-              builders to collaborate on the technical and regulatory research
-              of blockchain technologies that promote a web3-friendly regulatory
-              environment. The speed of blockchain innovation is outpacing the
-              regulatory environment and leaving legislators behind in
-              understanding and creating web3-friendly regulation.
-            </p>
-            <br />
-            <p style={{ color: "var(--content-color)", maxWidth: "550px" }}>
-              The underlying technology propelling the blockchain will
-              continuously advance regardless of any political regulation, and
-              for us to capture the benefits of emerging and innovative
-              blockchain technology, we need to understand how and where
-              regulation of the blockchain connect.
-            </p>
-          </div>
-        </div>
-      </div>
-      <style>{inlineStyles}</style>
+      <Footer />
     </div>
   );
 }
