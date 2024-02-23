f33135814b14a87ef82af623309c456f17744ae8
Michael <enjoyweaver@gmail.com>
2024-02-23 10:37:25 -0600

    home desk and mobile

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### This is the website for Crypto Policy DAO.
 
-### The website was made with Vite, React, and Typescript
+### The website was made with Vite, React, and Tailwind CSS (in some places).
 
 You can clone the this template by running the following:
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 
 export default function Home() {
   const [isZoomed, setIsZoomed] = useState(false);
-
   const inlineStyles = `
     video {
       transition: transform 0.5s ease-in-out; /* Smooth transition */
@@ -12,98 +11,105 @@ export default function Home() {
     video:hover {
       transform: scale(1.2); /* Zoomed scale - 1.2 times */
     }
-    .video-container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-
-    .header {
-      color: var(--header-color);
-      font-size: 2rem;
-    }
-
-    .sub-header {
-      color: var(--header-color);
-      font-size: 1.5rem;
-    }
-    
-    .content1 {
-      color: var(--description-color);
-      font-size: 1rem;
-    }
-
-    .content2 {
-      color: var(--content-color);
-      font-size: 1rem;
-    }
-
-    @media (max-width: 768px) {
-      .video-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      video {
-        width: 100%;
-        height: auto;
-      }
-      .header {
-        font-size: 1.5rem;
-      }
-      .sub-header {
-        font-size: 1.2rem;
-      }
-      .content1 {
-        font-size: 0.8rem;
-      }
-      .content2 {
-        font-size: 0.8rem;
-      }
-    }
   `;
-
   return (
-    <div className="container mx-auto pt-20 pb-20 justify-center items-center">
-      <p className="header pb-10">Crypto Policy DAO</p>
-      <div className="video-center video-container">
-        <video
-          autoPlay
-          loop
-          muted
-          width="490"
-          height="580"
-          onMouseEnter={() => setIsZoomed(true)}
-          onMouseLeave={() => setIsZoomed(false)}
-          className={isZoomed ? "zoomed" : ""}
-        >
-          <source src={videoFile} type="video/mp4" />
-        </video>
-      </div>
-      <div className=" text-center">
-        <br></br>
-        <p className="sub-header text-center">
-          Web3-friendly Regulatory Think Tank
-        </p>
+    <div className="container pt-20">
+      {/* Mobile layout (max-width: 768px) */}
+      <div className="md:hidden">
+        <div className="container mx-auto pt-20 pb-20 justify-center items-center">
+          <p className="header pb-10 ">Crypto Policy DAO</p>
+          <div className="video-center video-container">
+            <video
+              autoPlay
+              loop
+              muted
+              width="490"
+              height="580"
+              onMouseEnter={() => setIsZoomed(true)}
+              onMouseLeave={() => setIsZoomed(false)}
+              className={isZoomed ? "zoomed" : ""}
+            >
+              <source src={videoFile} type="video/mp4" />
+            </video>
+          </div>
+          <div className="text-center">
+            <br />
+            <p className="sub-header text-center ">
+              Web3-friendly Regulatory Think Tank
+            </p>
 
-        <br></br>
-        <div className="text-center pl-40 pr-40">
-          <p className="content1">
-            Our mission is to be a platform for policymakers and blockchain
-            builders to collaborate on the technical and regulatory research of
-            blockchain technologies that promote a web3-friendly regulatory
-            environment. The speed of blockchain innovation is outpacing the
-            regulatory environment and leaving legislators behind in
-            understanding and creating web3-friendly regulation.
+            <br />
+            <div className="text-center pl-40 pr-40">
+              <p className="content1">
+                Our mission is to be a platform for policymakers and blockchain
+                builders to collaborate on the technical and regulatory research
+                of blockchain technologies that promote a web3-friendly
+                regulatory environment. The speed of blockchain innovation is
+                outpacing the regulatory environment and leaving legislators
+                behind in understanding and creating web3-friendly regulation.
+              </p>
+              <br />
+              <p className="content2">
+                The underlying technology propelling the blockchain will
+                continuously advance regardless of any political regulation, and
+                for us to capture the benefits of emerging and innovative
+                blockchain technology, we need to understand how and where
+                regulation of the blockchain connect.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Desktop layout (min-width: 768px) */}
+      <div className="pt-10 hidden md:flex">
+        <div className="w-1/2 text-center pl-20">
+          <video
+            autoPlay
+            loop
+            muted
+            width="490"
+            height="580"
+            onMouseEnter={() => setIsZoomed(true)}
+            onMouseLeave={() => setIsZoomed(false)}
+            className={isZoomed ? "zoomed" : ""}
+          >
+            <source src={videoFile} type="video/mp4" />
+          </video>
+        </div>
+        <div className="w-1/2 text-left max-w-600">
+          <p
+            className="text-left text-2xl"
+            style={{ color: "var(--header-color)" }}
+          >
+            Crypto Policy DAO
           </p>
           <br />
-          <p className="content2">
-            The underlying technology propelling the blockchain will
-            continuously advance regardless of any political regulation, and for
-            us to capture the benefits of emerging and innovative blockchain
-            technology, we need to understand how and where regulation of the
-            blockchain connect.
+          <p
+            className="text-left text-xl"
+            style={{ color: "var(--header-color)" }}
+          >
+            Web3-friendly Regulatory Think Tank
           </p>
+          <br />
+          <div className="text-left">
+            <p style={{ color: "var(--description-color)" }}>
+              Our mission is to be a platform for policymakers and blockchain
+              builders to collaborate on the technical and regulatory research
+              of blockchain technologies that promote a web3-friendly regulatory
+              environment. The speed of blockchain innovation is outpacing the
+              regulatory environment and leaving legislators behind in
+              understanding and creating web3-friendly regulation.
+            </p>
+            <br />
+            <p style={{ color: "var(--content-color)" }}>
+              The underlying technology propelling the blockchain will
+              continuously advance regardless of any political regulation, and
+              for us to capture the benefits of emerging and innovative
+              blockchain technology, we need to understand how and where
+              regulation of the blockchain connect.
+            </p>
+          </div>
         </div>
       </div>
       <style>{inlineStyles}</style>
